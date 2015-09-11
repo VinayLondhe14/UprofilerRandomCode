c828ae59a5d4b910b1bea5aaad4148ba32d8e278
VinayLondhe14 <vlondhe@seas.upenn.edu>
2015-09-11 16:51:38 -0400

    First commit

---
+++ UprofilerExample.php
@@ -0,0 +1,130 @@
+<?php
+//require 'vendor/autoload.php';
+//
+//use Aws\DynamoDb\DynamoDbClient;
+//
+//$client = DynamoDbClient::factory(array(
+//    'profile' => 'project1',
+//    'version' => 'latest',
+//    'region' => 'us-west-2', #replace with your desired region
+//    'endpoint' => 'http://localhost:8000'
+//));
+//
+////// Create an "errors" table
+////$client->createTable(array(
+////  'TableName' => 'errors',
+////  'AttributeDefinitions' => array(
+////    array(
+////      'AttributeName' => 'id',
+////      'AttributeType' => 'N'
+////    ),
+////    array(
+////      'AttributeName' => 'time',
+////      'AttributeType' => 'N'
+////    )
+////  ),
+////  'KeySchema' => array(
+////    array(
+////      'AttributeName' => 'id',
+////      'KeyType'       => 'HASH'
+////    ),
+////    array(
+////      'AttributeName' => 'time',
+////      'KeyType'       => 'RANGE'
+////    )
+////  ),
+////  'ProvisionedThroughput' => array(
+////    'ReadCapacityUnits'  => 10,
+////    'WriteCapacityUnits' => 20
+////  )
+////));
+//
+//$result = $client->listTables();
+//
+//// TableNames contains an array of table names
+//foreach ($result['TableNames'] as $tableName) {
+//  echo $tableName . "\n";
+//}
+//
+//$result = $client->describeTable(array(
+//  'TableName' => 'errors'
+//));
+//
+//// The result of an operation can be used like an array
+//echo $result['Table']['ItemCount'] . "\n";
+////> 0
+//
+//// Use the getPath() method to retrieve deeply nested array key values
+//echo $result->getPath('Table/ProvisionedThroughput/ReadCapacityUnits') . "\n";
+////> 15
+//
+//
+//$time = time();
+//
+//$result = $client->putItem(array(
+//  'TableName' => 'errors',
+//  'Item' => array(
+//    'id'      => array('N' => '1201'),
+//    'time'    => array('N' => $time),
+//    'error'   => array('S' => 'Executive overflow'),
+//    'message' => array('S' => 'no vacant areas')
+//  )
+//));
+//
+//$result = $client->getItem(array(
+//  'ConsistentRead' => true,
+//  'TableName' => 'errors',
+//  'Key'       => array(
+//    'id'   => array('N' => '1201'),
+//    'time' => array('N' => $time)
+//  )
+//));
+//
+//// Grab value from the result object like an array
+//echo $result['Item']['id']['N'] . "\n";
+////> 1201
+//echo $result->getPath('Item/id/N') . "\n";
+////> 1201
+//echo $result['Item']['error']['S'] . "\n";
+////> Executive overflow
+//echo $result['Item']['message']['S'] . "\n";
+//
+
+
+//$uprofiler_ROOT = getcwd(). "/uprofiler";
+//include_once $uprofiler_ROOT . "/uprofiler_lib/utils/uprofiler_lib.php";
+//include_once $uprofiler_ROOT . "/uprofiler_lib/utils/uprofiler_runs.php";
+
+
+
+
+
+function bar($x) {
+  if ($x > 0) {
+    bar($x - 1);
+  }
+}
+
+function foo() {
+  for ($idx = 0; $idx < 2; $idx++) {
+    bar($idx);
+    $x = strlen("abc");
+  }
+}
+
+// start profiling
+//uprofiler_enable();
+include(dirname(__FILE__) . "/UprofilerGetSaveRuns.php");
+// run program
+foo();
+
+// stop profiler
+$uprofiler_data = uprofiler_disable();
+$uprofiler_runs = new UprofilerGetSaveRuns();
+$run_id = $uprofiler_runs->save_run($uprofiler_data, "brand_name");
+
+echo "---------------\n".
+  "Assuming you have set up the http based UI for \n".
+  "uprofiler at some address, you can view run at \n".
+  "http://<uprofiler-ui-address>/index.php?run=$run_id&source=brand_name\n".
+  "---------------\n";
---
+++ UprofilerGetSaveRuns.php
@@ -0,0 +1,172 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Aws\DynamoDb\DynamoDbClient;
+use Aws\DynamoDb\Exception\ResourceNotFoundException;
+use Aws\S3\S3Client;
+
+include(dirname(__FILE__) . "/uprofiler/uprofiler_lib/utils/uprofiler_lib.php");
+include(dirname(__FILE__) . "/uprofiler/uprofiler_lib/utils/uprofiler_runs.php");
+
+class UprofilerGetSaveRuns implements \iUprofilerRuns {
+
+  private $tableName = 'pc2';
+  private $bucketName = 'pubcentral';
+  public function __construct($tableName = "pc2", $bucketName = "pubcentral") {
+    $this->tableName = $tableName;
+    $this->bucketName = $bucketName;
+  }
+
+  private function gen_run_id($type) {
+    return uniqid();
+  }
+
+  private function getDynamoDbClient() {
+
+    $client = DynamoDbClient::factory(array(
+      'profile' => 'project1',
+      'version' => 'latest',
+      'region' => 'us-west-2', #replace with your desired region
+      'endpoint' => 'http://localhost:8000'
+    ));
+    return $client;
+  }
+
+  private function getAmazonS3Client() {
+    $client = S3Client::factory(array(
+      'profile' => 'project2'
+    ));
+    return $client;
+  }
+
+  private function checkIfTableExists($client) {
+
+    try {
+      $result = $client->describeTable(array(
+        "TableName" => $this->tableName
+      ));
+    } catch (ResourceNotFoundException $e) {
+      // if this exception is thrown, the table doesn't exist
+      return false;
+    }
+
+    // no exception thrown? table exists!
+    return true;
+  }
+
+  private function checkIfBucketExists($client) {
+
+    return $client->doesBucketExist($this->bucketName);
+  }
+
+  private function createDynamoDbTable($client) {
+
+    $client->createTable(array(
+      'TableName' => $this->tableName,
+      'AttributeDefinitions' => array(
+        array(
+          'AttributeName' => 'id',
+          'AttributeType' => 'S'
+        )
+      ),
+      'KeySchema' => array(
+        array(
+          'AttributeName' => 'id',
+          'KeyType'       => 'HASH'
+        )
+      ),
+      'ProvisionedThroughput' => array(
+        'ReadCapacityUnits'  => 10,
+        'WriteCapacityUnits' => 20
+      )
+    ));
+
+//    $client->waitUntil('TableExists', array(
+//      'TableName' => $this->tableName
+//    ));
+  }
+
+  private function createAmazonS3Bucket($client) {
+    $client->createBucket(array('Bucket' => $this->bucketName));
+    $client->waitUntil('BucketExists', array('Bucket' => $this->bucketName));
+  }
+
+  private function putItemInDynamoDb($client, $id, $brand_name) {
+
+    $result = $client->putItem(array(
+      'TableName' => $this->tableName,
+      'Item' => array(
+        'id' => array('S' => $id),
+        'brand_name' => array('S' => $brand_name)
+      )
+    ));
+  }
+
+  private function putObjectInAmazonS3($client, $id, $uprofiler_data) {
+    $result = $client->putObject(array(
+      'Bucket' => $this->bucketName,
+      'Key'    => "uprofiler/" . $id . ".txt",
+      'Body'   => serialize($uprofiler_data)
+    ));
+  }
+
+  public function get_run($run_id, $type = null, &$run_desc = null) {
+
+    $s3client = $this->getAmazonS3Client();
+//    $result = $client->getItem(array(
+//      'ConsistentRead' => true,
+//      'TableName' => $this->tableName,
+//      'Key'       => array(
+//        'id'   => array('S' => $run_id)
+//      )
+//    ));
+    $result = $s3client->getObject(array(
+      'Bucket' => $this->bucketName,
+      'Key'    => "uprofiler/" . $run_id . ".txt"
+    ));
+    //echo $result['Item']['id']['S'] . "\n";
+    //echo $result['Item']['uprofiler_data']['S'];
+    return unserialize($result['Body']);
+  }
+
+  public function save_run($uprofiler_data, $brand_name, $run_id = null) {
+
+    if ($run_id === null) {
+      $run_id = $this->gen_run_id($brand_name);
+    }
+    $ddbclient = $this->getDynamoDbClient();
+    if(!$this->checkIfTableExists($ddbclient)) {
+      $this->createDynamoDbTable($ddbclient);
+    }
+    $this->putItemInDynamoDb($ddbclient, $run_id, $brand_name);
+
+    $s3client = $this->getAmazonS3Client();
+    if(!$this->checkIfBucketExists($s3client)) {
+      $this->createAmazonS3Bucket($s3client);
+    }
+    $this->putObjectInAmazonS3($s3client, $run_id, $uprofiler_data);
+    //echo $run_id;
+    //$result = $ddbclient->listTables();
+    return $run_id;
+  }
+
+
+  public function list_runs() {
+
+    $client = $this->getDynamoDbClient();
+    $response = $client->scan(array(
+      'TableName' => $this->tableName
+    ));
+    echo "<br>";
+    echo "The following id's are available:" . "<br>";
+    foreach ($response['Items'] as $key => $value) {
+      echo 'Id: ' . $value['id']['S'] . str_repeat('&nbsp;', 20) . 'Brand Name:' . $value['brand_name']['S'] . "<br>";
+    }
+    echo "---------------\n".
+      "Assuming you have set up the http based UI for \n".
+      "uprofiler at some address, you can view run at \n".
+      "http://<uprofiler-ui-address>/index.php?run=Id&source=brand_name\n".
+      "---------------\n";
+  }
+}
